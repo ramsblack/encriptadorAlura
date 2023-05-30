6999e67720f8abcfde7a8b8e8a6a547d004f1934
Oscar <ramsblack@gmail.com>
2023-05-30 15:57:09 -0500

    Incluyendo configuracion javascript para encriptar y validacion de texto

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-//const textoEncriptado = document.querySelector('.textoEncrip')
+document.getElementById("btncopiar").style.display = 'none';
 
-function validarTexto(){
-    let texto = document.querySelector('.textoEncrip').value;
-    let acentos = /^[a-zA-Z]+(\s*[a-zA-Z]*)*[a-zA-Z]+$/;
-    let validador = texto.match(acentos);
+function encriptar(){
+    const caracter = /[áéíóúàèìòù'A-Z0-9~!@#$%&*()_+|{}[\]\\\/?><^:"`]/;
+    let texto = document.querySelector(".textoEncrip").value;
+    if (texto == ""){
+        alert("El capo no puede estar vacio");
+    }else if(texto.match(caracter) != texto.match(caracter)){
+        alert('El texto no puede contener acentos ni caracteres especiales');
+        document.querySelector(".textoEncrip").value = "";
+    } else{
+        let textoEncript = texto.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat")
+        console.log(textoEncript);
 
-    if(!validador || validador === 0) {
-        alert("Solo son permitidas letras minúsculas y sin acentos")
-        location.reload();
-        return true;
+        document.getElementById("textoResultado").innerHTML = textoEncript;
+        document.getElementById("immuneco").style.display = 'none';
+        document.getElementById("btncopiar").style.display = 'inline';
+        document.getElementById("textoNoEncontrado").style.display = 'none';
+        document.getElementById("textoNoEncontrado2").style.display = 'none';
+
+
     }
 }
 
-    
-function encriptarTexto(){
-    
+function btncopiar(){
+    let copiarTexto = document.getElementById("textoResultado").value;
+    navigator.clipboard.writeText(copiarTexto);
+    alert("Texto Copiado");
 }
-
-
+  
